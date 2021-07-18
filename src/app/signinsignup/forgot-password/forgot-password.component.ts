630954011571c0bcdfafc6ee10b98c343ed40e59
artart37 <artgalichyan@gmail.com>
2021-07-19 01:39:18 +0400

    li text wrap through display table (footer), forgotpassword, back button

--- src/app/signinsignup/forgot-password/forgot-password.component.ts
+++ src/app/signinsignup/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,7 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent {
+ perfectcentre:boolean = true;
 
-  constructor() { }
+
+back():void {
+  try {
+    this.location.back()
+  } catch {
+    this.router.navigateByUrl("/")
+  }
+ }
+ constructor(private router: Router, private location:Location) {  }
 
 }
