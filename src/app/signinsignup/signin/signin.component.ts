96e279709a44be1cf692e12f1e6f45f95fbf5cd9
artart37 <artgalichyan@gmail.com>
2021-08-04 23:56:17 +0400

    signin dynamic validation

--- src/app/signinsignup/signin/signin.component.ts
+++ src/app/signinsignup/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+import { emailvalidation, passwordvalidation } from '../directives/customvalidation/emailpassword';
 
 @Component({
   selector: 'app-signin',
@@ -6,9 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
-  nosigninheader:boolean = false;
-  perfectcentre:boolean = true;
+  nosigninheader: boolean = false;
+  perfectcentre: boolean = true;
+  //Reactive Form
+  signingroup = new FormGroup({
+    email: new FormControl('', {
+      validators: emailvalidation,
+      updateOn: "blur"
+    }),
+    password: new FormControl('', {
+      validators: passwordvalidation,
+      updateOn: "blur"
+    })
+  });
+  emailMessage: string | null = "";
+  passwordMessage: string | null = "";
+
+  get email() {
+    return this.signingroup.get("email")
+  }
+  get password() {
+    return this.signingroup.get("password")
+  }
+
+  get showPassError() {
+    return this.password?.invalid && this.password?.dirty
+  }
+
+  get showEmailError() {
+    return this.email?.invalid && this.email?.dirty
+  }
+
+  //Submit the form
+  signin() { }
 
   constructor() { }
-
 }
