a5430df40bcda9d464ce77399a22b17bbb8cb356
Aramayis Galichyan <aramayisgalichyan@gmail.com>
2022-04-17 16:44:54 +0400

    Set up unit testing for the header component

--- src/app/static/header-footer/header/header.component.spec.ts
+++ src/app/static/header-footer/header/header.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ButtonsModule } from '../../../shared/components/button/modules/buttons.module';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,9 +10,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, ButtonsModule],
+      declarations: [HeaderComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
